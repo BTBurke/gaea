7584bd3efd5a0802c871d6714b561b8c500a0746
Bryan Burke <bryan.t.burke@gmail.com>
2015-06-07 20:34:47 +0800

    menu and announcements on home

--- app/scripts/components/topnav.js
+++ app/scripts/components/topnav.js
@@ -8,11 +8,10 @@ class TopNav extends React.Component {
 
   render() {
     return (
-      <B.Navbar brand='React-Bootstrap' toggleNavKey={0}>
+      <B.Navbar toggleNavKey={0} brand={'Guangzhou American Employee Association'}>
+
         <B.Nav right eventKey={0}> {/* This is the eventKey referenced */}
-          <B.NavItem eventKey={1} href='#'>Events</B.NavItem>
-          <B.NavItem eventKey={2} href='#'>Order</B.NavItem>
-          <B.DropdownButton eventKey={3} title={this.props.user}>
+          <B.DropdownButton eventKey={1} title={this.props.user}>
             <B.MenuItem eventKey='1'>Profile</B.MenuItem>
             <B.MenuItem eventKey='2'>Logout</B.MenuItem>
           </B.DropdownButton>
--- app/scripts/pages/home.js
+++ app/scripts/pages/home.js
@@ -3,6 +3,7 @@
 var Marty = require('marty');
 var UserStore = require('../stores/userstore');
 var React = require('react');
+var B = require('react-bootstrap');
 
 var TopNav = require('../components/topnav');
 
@@ -16,21 +17,32 @@ class Home extends React.Component {
     return (
       <div>
       <TopNav user={this.props.user.userName}/>
-      <div>First: {this.props.user.firstName}</div>
-      <div>Last: {this.props.user.lastName}</div>
+      <B.Grid>
+        <B.Row>
+          <B.Col md={3} lg={3}>
+            <B.ListGroup>
+              <B.ListGroupItem href="/#/order">Submit an Order</B.ListGroupItem>
+              <B.ListGroupItem href="/#/events">See Schedule of Events</B.ListGroupItem>
+            </B.ListGroup>
+          </B.Col>
+
+          <B.Col md={9} lg={9}>
+
+            <B.Panel header='Use your duty-free liquor benefit'>
+              <p>The next duty-free liquor order is coming up.  We expect that all orders will have to be in by the end of July for a delivery date in August.</p>
+              <B.Button bsStyle='success' bsSize='small' href='/#/order'>Submit an order</B.Button>
+              <div className="home-posted-on">Posted on 5-June-2015</div>
+
+            </B.Panel>
+          </B.Col>
+        </B.Row>
+      </B.Grid>
+
       </div>
     );
   }
 }
 
-// module.exports = Marty.createContainer(Home, {
-//   listenTo: UserStore,
-//   fetch: {
-//     user() {
-//       return UserStore.getUser();
-//     }
-//   }
-// });
 
 module.exports = Marty.createContainer(Home, {
   listenTo: 'UserStore',
