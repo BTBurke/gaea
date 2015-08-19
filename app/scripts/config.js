da900024fda94c3be4e915874468066b28e4acf6
Bryan Burke <bryan.t.burke@gmail.com>
2015-08-19 10:01:29 +0800

    new favicon, cleanup old flux

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,17 +1,17 @@
 // For local development
-// var Config = {
-//   'baseURL': 'http://127.0.0.1:8080',
-//   'homeURL': 'http://127.0.0.1:8081',
-//   'logLevel': 'debug',
-//   'inventory_required': ['supplier_id', 'name', 'nonmem_price', 'mem_price', 'types']
-// }
+var Config = {
+  'baseURL': 'http://127.0.0.1:8080',
+  'homeURL': 'http://127.0.0.1:8081',
+  'logLevel': 'debug',
+  'inventory_required': ['supplier_id', 'name', 'nonmem_price', 'mem_price', 'types']
+}
 
 // For c9 development
-var Config = {
-    'baseURL': 'http://gserver-btburke75.c9.io',
-    'homeURL': 'http://gaea-btburke75.c9.io',
-    'logLevel': 'debug',
-    'inventory_required': ['supplier_id', 'name', 'nonmem_price', 'mem_price', 'types']
-}
+// var Config = {
+//     'baseURL': 'http://gserver-btburke75.c9.io',
+//     'homeURL': 'http://gaea-btburke75.c9.io',
+//     'logLevel': 'debug',
+//     'inventory_required': ['supplier_id', 'name', 'nonmem_price', 'mem_price', 'types']
+// }
 
 module.exports = Config;
