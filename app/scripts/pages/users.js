cd0386e31a209db64bbaa841cdcb9ae467267d53
Bryan Burke <bryan.t.burke@gmail.com>
2015-08-31 21:15:58 +0800

    spec out users page

---
+++ app/scripts/pages/users.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var B = require('react-bootstrap');
+var _ = require('underscore');
+
+var TopNav = require('../components/topnav');
+var log = require('../services/logger');
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <TopNav user={this.props.user.fullName} />
+        <B.Grid>
+          <B.Row>
+            <B.Col md={3} lg={3}>
+            </B.Col>
+            <B.Col md={9} lg={9}>
+              <div className="users-stats">
+                {userStats}
+              </div>
+              <div className="users-table">
+              </div>
+            </B.Col>
+          <B.Row>
+        </B.Grid>
+      </div>
+    );
+  }
+}
