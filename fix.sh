c6ac19de9b3cd6785b30398ec65233a519cacdde
Bryan Burke <bryan.t.burke@gmail.com>
2015-09-04 04:11:39 +0000

    fix for new clone

---
+++ fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p app/bower_components/jquery/dist
+cp bower_components/jquery/dist/* app/bower_components/jquery/dist
+
+mkdir -p app/bower_components/bootstrap-sass-official/assets/fonts
+cp bower_components/bootstrap-sass-official/assets/fonts/bootstrap/* app/bower_components/bootstrap-sass-official/assets/fonts/
